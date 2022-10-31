b6398d6cb99fd776a1b4d1ce0d5afa4fbbbfeb10
abdelrahman <gadoua50@gmail.com>
2022-10-31 07:50:55 +0200

    finish section 12

---
+++ eventDB/04-query-data.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM events
+-- WHERE date_palnned >= '2022-06-01' OR min_age = 20;
+
+
+SELECT * FROM events
+ORDER BY id DESC;
---
+++ eventDB/drop.sql
@@ -0,0 +1,7 @@
+DROP TABLE cities;
+DROP TABLE events;
+DROP TABLE events_users;
+DROP TABLE locations;
+DROP TABLE organizers;
+DROP TABLE tags;
+DROP TABLE users;
